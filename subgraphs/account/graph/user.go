d32d1afce7e6a2bbbc180bb3a96de43b64e8e36b
ichikawashingo <ichikawa.shingo.0829@gmail.com>
2025-03-13 14:36:49 +0900

    add MVP

---
+++ subgraphs/account/graph/user.go
@@ -0,0 +1,21 @@
+package graph
+
+import "account/graph/model"
+
+var users = []*model.User{
+	{
+		ID:    "asdf",
+		Name:  "Icchy",
+		Email: "<EMAIL>",
+	},
+	{
+		ID:    "1",
+		Name:  "John",
+		Email: "<EMAIL>",
+	},
+	{
+		ID:    "2",
+		Name:  "Jane",
+		Email: "<EMAIL>",
+	},
+}
---
+++ subgraphs/todo/graph/todo.go
@@ -0,0 +1,5 @@
+package graph
+
+import "todo/graph/model"
+
+var todoMap = make(map[string]*model.Todo)
